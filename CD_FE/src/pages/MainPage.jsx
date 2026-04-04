29230771e9b26d0c0b4c9b12327f5745f0a28b67
Kim Minju <myng25@naver.com>
2026-04-04 14:01:35 +0900

    feat: 메인 페이지 작성 및 App.jsx 라우팅

---
+++ CD_FE/src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+function MainPage() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-[#efefef] text-center gap-4">
+      <h1 className="text-3xl font-bold px-10">웹캠으로 면접 때 버릇을 알아보자</h1>
+      <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        로그인
+      </button>
+      <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        회원가입
+      </button>
+    </div>
+  )
+}
+
+export default MainPage
