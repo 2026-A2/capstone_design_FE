8aec8c8d9b8cea22853badf85d44d4d9bd890dbf
양준원 <cherami8@hufs.ac.kr>
2026-05-12 03:25:52 +0900

    feat: mockdata 값 통일

--- CD_FE/src/mockdata/report/trendMock.js
+++ CD_FE/src/mockdata/report/trendMock.js
@@ -1,66 +1,60 @@
 export const speechRateTrend = [
-  { session: 1, value: 132 },
-  { session: 2, value: 145 },
-  { session: 3, value: 151 },
+  { session: 1, value: 200 },
+  { session: 2, value: 230 },
+  { session: 3, value: 280 },
 ];
 
 export const voiceVolumeTrend = [
-  { session: 1, avgVoiceVolume: -28 },
-  { session: 2, avgVoiceVolume: -32 },
+  { session: 1, avgVoiceVolume: -15 },
+  { session: 2, avgVoiceVolume: -25 },
   { session: 3, avgVoiceVolume: -18 },
-  { session: 4, avgVoiceVolume: -40 },
 ];
 
 export const silenceTrend = [
-  { session: 1, value: 5 },
-  { session: 2, value: 4 },
-  { session: 3, value: 3 },
+  { session: 1, value: 2 },
+  { session: 2, value: 5 },
+  { session: 3, value: 7 },
 ];
 
 export const fillerTrend = [
-  { session: 1, value: 7 },
-  { session: 2, value: 5 },
-  { session: 3, value: 4 },
+  { session: 1, value: 5 },
+  { session: 2, value: 7 },
+  { session: 3, value: 1 },
 ];
 
 export const smileTrend = [
-  { session: 1, value: 28 },
-  { session: 2, value: 35 },
-  { session: 3, value: 41 },
+  { session: 1, value: 42 },
+  { session: 2, value: 51 },
+  { session: 3, value: 58 },
 ];
 
 export const eyeContactTrend = [
-  { session: 1, value: 61 },
+  { session: 1, value: 59 },
   { session: 2, value: 68 },
   { session: 3, value: 74 },
 ];
 export const blinkTrend = [
-  { session: 1, value: 22 },
-  { session: 2, value: 18 },
-  { session: 3, value: 16 },
-  { session: 4, value: 19 },
+  { session: 1, value: 18 },
+  { session: 2, value: 15 },
+  { session: 3, value: 13 },
 ];
 export const endingBlurTrend = [
-  { session: 1, value: 22 },
-  { session: 2, value: 28 },
-  { session: 3, value: 18 },
-  { session: 4, value: 15 },
+  { session: 1, value: 26 },
+  { session: 2, value: 22 },
+  { session: 3, value: 19 },
 ];
 export const nodTrend = [
-  { session: 1, value: 88 },
-  { session: 2, value: 75 },
-  { session: 3, value: 92 },
-  { session: 4, value: 90 },
+  { session: 1, value: 82 },
+  { session: 2, value: 90 },
+  { session: 3, value: 77 },
 ];
 export const shoulderTiltTrend = [
-  { session: 1, value: 92 },
-  { session: 2, value: 88 },
-  { session: 3, value: 75 },
-  { session: 4, value: 65 },
+  { session: 1, value: 77 },
+  { session: 2, value: 82 },
+  { session: 3, value: 70 },
 ];
 export const bodyShakeTrend = [
-  { session: 1, value: 0 },
+  { session: 1, value: 3 },
   { session: 2, value: 2 },
   { session: 3, value: 1 },
-  { session: 4, value: 3 },
 ];
